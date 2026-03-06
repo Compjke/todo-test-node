3695b9a9b21b774876f3f4b521dbf30fbf1b2b7b
Oleksandr Suvorov <compjkee@gmail.com>
2026-03-06 21:36:08 +0200

    add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import { connectToDb } from './config/connectDb.js';
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
+import { connectToDb } from './config/connectDb.js';
 import authRoutes from './routes/authRoutes.js';
 import todoRoutes from './routes/todoRoutes.js';
 import translationRoutes from './routes/translationRoutes.js';
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(helmet());
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: ['https://todo-react-for-node-js.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+}));
 const authLimiter = rateLimit({
   windowMs: 10000,
   max: 5,
